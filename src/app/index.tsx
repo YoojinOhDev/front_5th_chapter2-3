4fb306a61af7d3c3a9db9453a2083dadecafaa3c
yoojinDev <yj.oh@teamsparta.co>
2025-04-27 21:37:24 +0900

    src/app 폴더에 기본 index.tsx 생성 후 react 기본 render에서 호출

---
+++ src/app/index.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router } from "react-router-dom"
+import Header from "../widgets/ui/Header.tsx"
+import Footer from "../widgets/ui/Footer.tsx"
+import PostsManagerPage from "../pages/PostsManagerPage.tsx"
+import React from "react"
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </React.StrictMode>
+  )
+}
+
+export default App;
