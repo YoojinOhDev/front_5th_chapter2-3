07b43faa560c8ed9c03fc7433f98243eaeee7ce0
yoojinDev <yj.oh@teamsparta.co>
2025-04-27 22:28:03 +0900

    vitest에서도 alias 별칭 추가

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,15 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
 })
