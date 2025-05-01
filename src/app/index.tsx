be1c627bdcd01af4b9aa40ac2c56028bb8712f41
yoojinDev <yj.oh@teamsparta.co>
2025-05-01 11:50:26 +0900

    react query 설치 후 queryProvider 적용

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import {PostsManagerPage} from "@/pages/post/ui/PostsManagerPage.tsx"
+import { PostsManagerPage } from "@/pages/post/ui/PostsManagerPage.tsx"
 import React from "react"
+import { QueryProvider } from "@/app/providers/QueryProvider.tsx"
 
 const App = () => {
   return (
     <React.StrictMode>
       <Router>
-        <PostsManagerPage />
+        <QueryProvider>
+          <PostsManagerPage />
+        </QueryProvider>
       </Router>
     </React.StrictMode>
   )
